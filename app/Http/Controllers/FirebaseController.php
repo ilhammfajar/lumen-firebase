59268531a6f42efba3518f9c508fa6851eda5170
ilhammfajar <ilhammfwork@gmail.com>
2021-10-24 18:56:01 +0700

    push one

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Auth;
+use Firebase\Auth\Token\Exception\InvalidToken;
+use Kreait\Firebase\Exception\Auth\RevokedIdToken;
+
+class FirebaseController extends Controller
+{
+    protected $auth, $database;
+
+    public function __construct()
+    {
+        $factory = (new Factory)
+        ->withServiceAccount(__DIR__.'/crudlumen-firebase-adminsdk-nw674-79d564b135.json')
+        ->withDatabaseUri('https://crudlumen-default-rtdb.firebaseio.com/');
+
+        $this->auth = $factory->createAuth();
+        $this->database = $factory->createDatabase();
+    }
+
+    public function signUp()
+    {
+        $email = "ilhammfwork@gmail.com";
+        $pass = "test123123";
+
+        try {
+            $newUser = $this->auth->createUserWithEmailAndPassword($email, $pass);
+            dd($newUser);
+        } catch (\Throwable $e) {
+            switch ($e->getMessage()) {
+                case 'The email address is already in use by another account.':
+                    dd("Email sudah digunakan.");
+                    break;
+                case 'A password must be a string with at least 6 characters.':
+                    dd("Kata sandi minimal 6 karakter.");
+                    break;
+                default:
+                    dd($e->getMessage());
+                    break;
+            }
+        }
+    }
+
+    public function signIn()
+    {
+        $email = "ilhammfwork@gmail.com";
+        $pass = "test123123";
+
+        try {
+            $signInResult = $this->auth->signInWithEmailAndPassword($email, $pass);
+            // dump($signInResult->data());
+
+            Session::put('firebaseUserId', $signInResult->firebaseUserId());
+            Session::put('idToken', $signInResult->idToken());
+            Session::save();
+
+            dd($signInResult);
+        } catch (\Throwable $e) {
+            switch ($e->getMessage()) {
+                case 'INVALID_PASSWORD':
+                    dd("Kata sandi salah!.");
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    dd("Email tidak ditemukan.");
+                    break;
+                default:
+                    dd($e->getMessage());
+                    break;
+            }
+        }
+    }
+
+    public function signOut()
+    {
+        if (Session::has('firebaseUserId') && Session::has('idToken')) {
+            // dd("User masih login.");
+            $this->auth->revokeRefreshTokens(Session::get('firebaseUserId'));
+            Session::forget('firebaseUserId');
+            Session::forget('idToken');
+            Session::save();
+            dd("User berhasil logout.");
+        } else {
+            dd("User belum login.");
+        }
+    }
+
+    public function userCheck()
+    {
+        // $idToken = "";
+
+        // $this->auth->revokeRefreshTokens("");
+
+        // if (Session::has('firebaseUserId') && Session::has('idToken')) {
+        //     dd("User masih login.");
+        // } else {
+        //     dd("User sudah logout.");
+        // }
+
+        try {
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken, $checkIfRevoked = true);
+            dump($verifiedIdToken);
+            dump($verifiedIdToken->claims()->get('sub')); // uid
+            dump($this->auth->getUser($verifiedIdToken->claims()->get('sub')));
+        } catch (\Throwable $e) {
+            dd($e->getMessage());
+        }
+
+        // try {
+        //     $verifiedIdToken = $this->auth->verifyIdToken(Session::get('idToken'), $checkIfRevoked = true);
+        //     $response = "valid";
+        //     // dd("Valid");
+        //     // $uid = $verifiedIdToken->getClaim('sub');
+        //     // $user = $auth->getUser($uid);
+        //     // dump($uid);
+        //     // dump($user);
+        // } catch (\InvalidArgumentException $e) {
+        //     // dd('The token could not be parsed: '.$e->getMessage());
+        //     $response = "The token could not be parsed: " . $e->getMessage();
+        // } catch (InvalidToken $e) {
+        //     // dd('The token is invalid: '.$e->getMessage());
+        //     $response = "The token is invalid: " . $e->getMessage();
+        // } catch (RevokedIdToken $e) {
+        //     $response = "revoked";
+        // } catch (\Throwable $e) {
+        //     if (substr(
+        //         $e->getMessage(),
+        //         0,
+        //         21
+        //     ) == "This token is expired") {
+        //         $response = "expired";
+        //     } else {
+        //         $response = "something_wrong";
+        //     }
+        // }
+        // return $response;
+    }
+
+    public function read()
+    {
+        $ref = $this->database->getReference('hewan/herbivora/domba')->getSnapshot();
+        dump($ref);
+        $ref = $this->database->getReference('hewan/herbivora')->getValue();
+        dump($ref);
+        $ref = $this->database->getReference('hewan/karnivora')->getValue();
+        dump($ref);
+        $ref = $this->database->getReference('hewan/omnivora')->getSnapshot()->exists();
+        dump($ref);
+    }
+
+    public function update()
+    {
+        // before
+        $ref = $this->database->getReference('tumbuhan/dikotil')->getValue();
+        dump($ref);
+
+        // update data
+        $ref = $this->database->getReference('tumbuhan')
+        ->update(["dikotil" => "mangga"]);
+
+        // after
+        $ref = $this->database->getReference('tumbuhan/dikotil')->getValue();
+        dump($ref);
+    }
+
+    public function set()
+    {
+        // before
+        $ref = $this->database->getReference('hewan')->getValue();
+        dump($ref);
+
+        // set data
+        $ref = $this->database->getReference('hewan/karnivora')
+        ->set([
+            "harimau" => [
+                "benggala" => "galak",
+                "sumatera" => "jinak"
+            ]
+        ]);
+
+        // after
+        $ref = $this->database->getReference('hewan')->getValue();
+        dump($ref);
+    }
+
+    public function delete()
+    {
+        // before
+        $ref = $this->database->getReference('hewan/karnivora/harimau')->getValue();
+        dump($ref);
+
+        /**
+         * 1. remove()
+         * 2. set(null)
+         * 3. update(["key" => null])
+         */
+
+        // remove()
+        $ref = $this->database->getReference('hewan/karnivora/harimau/benggala')->remove();
+
+        // set(null)
+        $ref = $this->database->getReference('hewan/karnivora/harimau/benggala')
+            ->set(null);
+
+        // update(["key" => null])
+        $ref = $this->database->getReference('hewan/karnivora/harimau')
+            ->update(["benggala" => null]);
+
+        // after
+        $ref = $this->database->getReference('hewan/karnivora/harimau')->getValue();
+        dump($ref);
+    }
+
+}
